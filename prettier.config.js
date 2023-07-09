47c437df961532b321fcae339dee537836c8be87
Kazuki1131 <onetsubyo@gmail.com>
2023-07-09 23:22:18 +0900

    chore: add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 80,
+  singleQuote: false,
+  semi: false,
+  trailingComma: "all",
+  arrowParens: "always",
+
+  // @ianvs/prettier-plugin-sort-imports
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/components/(.*)$",
+    "^@/app/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "5.1.3",
+}
